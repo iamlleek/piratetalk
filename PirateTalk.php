3a4fcbad53c48f8f8feac399b7f9740717748972
iamlleek <66844593+iamlleek@users.noreply.github.com>
2020-11-18 14:59:53 -0500

    Add files via upload

---
+++ PirateTalk.php
@@ -0,0 +1,18 @@
+<!-- Chapter 1 example: Page 14 - 15 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>How to Talk Like a Pirate</title>
+</head>
+<body>
+	<h1>How to Talk Like a Pirate  </h1>
+	<?php
+		echo "Avast me hearties! ",
+		"Return handsomely with some fine swag, ye scurvy dogs! ",
+		"Else, we be keelhaulin' ye' next morn...";
+	?>
+</body>
+</html>
